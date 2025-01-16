d471e2e51986b7ce020a02ade0de1b4677c9927f
Aaron Dufour <thedufer@gmail.com>
2025-01-15 21:20:03 -0500

    additional thing for 40
    it seems that we don't need 3 remainders either; 2 will do. am I doing something wrong here?

--- set5/challenge40.ml
+++ set5/challenge40.ml
@@ -9,6 +9,27 @@ let encrypt_with_new_key =
     assert (Z.Compare.( < ) msg n);
     Rsa.encrypt pub msg, pub
 
+let%expect_test "only 2?" =
+  let c_0, `Pub (e_0, n_0) = encrypt_with_new_key () in
+  let c_1, `Pub (e_1, n_1) = encrypt_with_new_key () in
+  assert (Z.equal e_0 (Z.of_int 3));
+  assert (Z.equal e_1 (Z.of_int 3));
+  assert (Z.equal (Z.gcd n_0 n_1) Z.one);
+  let ( + ) = Z.( + ) in
+  let ( * ) = Z.( * ) in
+  let m_s_0 = n_1 in
+  let m_s_1 = n_0 in
+  let result =
+    Z.root
+      (Z.( mod )
+         (c_0 * m_s_0 * Z.invert m_s_0 n_0 +
+          c_1 * m_s_1 * Z.invert m_s_1 n_1)
+         (n_0 * n_1))
+      3
+  in
+  print_s [%sexp (Rsa.to_bits_trim result : string)];
+  [%expect {| "this is a secret that's much longer" |}]
+
 let%expect_test "final" =
   let c_0, `Pub (e_0, n_0) = encrypt_with_new_key () in
   let c_1, `Pub (e_1, n_1) = encrypt_with_new_key () in
@@ -34,4 +55,4 @@ let%expect_test "final" =
       3
   in
   print_s [%sexp (Rsa.to_bits_trim result : string)];
-  [%expect {||}]
+  [%expect {| "this is a secret that's much longer" |}]
